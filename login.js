d5904a59098e22a9e7990ddd0de43c21f72be3da
mayurbawne04-oss <mayurbawne04@gmail.com>
2025-10-30 06:20:52 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,8 @@
+// Subtle 3D movement on mouse move
+const card = document.querySelector('.card');
+
+document.addEventListener('mousemove', (e) => {
+  const x = (window.innerWidth / 2 - e.pageX) / 25;
+  const y = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+});
